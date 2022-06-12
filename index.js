3165dd5aa9433e5b3c9a667a1a39adcf68ff8d29
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-06-12 12:30:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// document.getElementById(  "demo"  ).innerHTML =  "Hello  Dolly."  ; 
+// var x = 2
+// let y = 4
+// const t = 5
+
+// if (true){
+//     var x = 5
+//     let y = 6
+//     const t = 6
+//     console.log(t)
+// }
+// console.log(x)
+// console.log(y)
+
+
+alert(  "Hello,Welcome!"  );  // Outputs: Hello World! 
+// Displaying a variable value 
+// var  x = 10; 
+// var  y = 20; 
+// var  sum = x + y; 
+// alert(sum);  // Out
