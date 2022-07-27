8052f261514eb3104f52fa988ed8bdab988eb11b
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-07-27 20:18:12 +0300

    changes

---
+++ DOM.js
@@ -0,0 +1,9 @@
+let div = document.getElementsByClassName("container")[0];
+console.log(div);
+
+let child = document.createElement("p");
+let features = document.createTextNode("Hello mum");
+child.appendChild(features)
+div.appendChild(child)
+ let head = document.querySelector("header")
+ console.log(head);
